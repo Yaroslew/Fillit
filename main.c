25be1101faee0d37ed6bf18e3f573ebf449d6d35
Pcorlys- <Yarosleu@gmail.com>
2019-02-16 12:07:56 +0300

    create_lst

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: pcorlys- <pcorlys-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/24 18:13:36 by pcorlys-          #+#    #+#             */
-/*   Updated: 2019/02/16 11:58:05 by pcorlys-         ###   ########.fr       */
+/*   Updated: 2019/02/16 12:00:55 by pcorlys-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,14 @@ int		main(int argc, char **argv)
 	root = root_area(count_figurs);
 	head = read_figurs(argv);
 	copy = head;
+	// твоя функция ниже.
 	head = name_your_function(head, copy, root);
 	while (head == NULL)
 	{
 		root++;
 		head = name_your_function(head, copy, root);
 	}
+
 
 
 
