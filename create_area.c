1bc81843408ceeabcdfb57026a80e8fae2df191f
Pcorlys- <Yarosleu@gmail.com>
2019-02-16 12:05:39 +0300

    create_lst

--- create_area.c
+++ create_area.c
@@ -1,35 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   create_area.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pcorlys- <pcorlys-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/24 18:13:36 by pcorlys-          #+#    #+#             */
-/*   Updated: 2019/02/16 11:58:05 by pcorlys-         ###   ########.fr       */
+/*   Created: 2019/02/16 12:01:11 by pcorlys-          #+#    #+#             */
+/*   Updated: 2019/02/16 12:01:11 by pcorlys-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "fillit.h"
 
-int		main(int argc, char **argv)
-{
-	int	count_figurs;
-	int	root;
-	t_crd	*head;
-	t_crd	*copy;
-
-	count_figurs = main_check(argc, argv);
-	root = root_area(count_figurs);
-	head = read_figurs(argv);
-	copy = head;
-	head = name_your_function(head, copy, root);
-	while (head == NULL)
-	{
-		root++;
-		head = name_your_function(head, copy, root);
-	}
-
-
-
-}
