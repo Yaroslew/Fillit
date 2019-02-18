6bc7216de2dd0e463e8fae950d96907cf001f2e9
Pcorlys- <Yarosleu@gmail.com>
2019-02-18 16:13:31 +0300

    18.02

--- root.c
+++ root.c
@@ -6,7 +6,7 @@
 /*   By: pcorlys- <pcorlys-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/23 07:23:49 by pcorlys-          #+#    #+#             */
-/*   Updated: 2019/02/18 15:43:44 by pcorlys-         ###   ########.fr       */
+/*   Updated: 2019/02/18 16:06:41 by pcorlys-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,13 @@ static int check_lst(t_crd *head)
 	flag = 0;
 	while (head)
 	{
-		if (head->y[0] == 0 &&  head->y[1] == 0 && head->y[2] == 0 && head->y[3] == 0)
-			flag++;
+		if ((head->y[1] == head->y[2] && (head->y[1] == head->y[0] || head->y[1] == head->y[3])
+		|| (head->x[1] == head->x[2] && (head->x[1] == head->x[0] || head->x[1] == head->x[3]))))
+			flag = 2;
+		if ((head->y[0] == head->y[1] &&  head->y[1] == head->y[2]
+		&& head->y[2] == head->y[3]) || (head->x[0] == head->x[1]
+		&& head->x[1] == head->x[2] && head->x[2] == head->x[3]))
+			flag = 1;
 		head = head->next;
 	}
 	return (flag);
@@ -37,7 +42,9 @@ int		root_area(int count, t_crd *head)
 	sum = count * 4;
 	while ((root * root) < sum)
 		root++;
-	if (root < 4 && flag != 0)
+	if (root < 4 && flag == 1)
 		root = 4;
+	if (root < 4 && flag == 2)
+		root = 3;
 	return (root);
 }
