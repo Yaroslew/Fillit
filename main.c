0535efc99c305ce414ae6d7fff30ebc83d55b5b2
Pcorlys- <Yarosleu@gmail.com>
2019-02-18 15:45:43 +0300

    18.02

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: pcorlys- <pcorlys-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/24 18:13:36 by pcorlys-          #+#    #+#             */
-/*   Updated: 2019/02/16 15:46:40 by pcorlys-         ###   ########.fr       */
+/*   Updated: 2019/02/18 15:39:19 by pcorlys-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,15 @@ int		main(int argc, char **argv)
 	int	count_figurs;
 	int	root;
 	t_crd	*head;
-	t_crd	*copy;
 
 	count_figurs = main_check(argc, argv);
 	head = read_figurs(argv);
 	root = root_area(count_figurs, head);
-//	copy = copy_lst(head);
-//	head = name_your_function(head, copy, root);
+//	head = name_your_function(head, root);
 	while (head == NULL)
 	{
 		root++;
-//		head = name_your_function(head, copy, root);
+//		head = name_your_function(head, root);
 	}
 	create_area(head, root);
 	free_lst(&head);
